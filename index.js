091d53ec0c563ef100d052eb2f72042c048dd674
Your Name <Rimon0000>
2023-06-12 21:20:44 +0600

    class deleted option done

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.port || 5000;
 
 //middleware
@@ -53,6 +53,13 @@ async function run() {
       const item = req.body
       console.log(item)
       const result = await cartCollection.insertOne(item)
+      res.send(result)
+    })
+
+    app.delete("/carts/:id", async(req, res) =>{
+      const id = req.params.id 
+      const query = { _id: new ObjectId(id) };
+      const result = await cartCollection.deleteOne(query)
       res.send(result)
     })
 
