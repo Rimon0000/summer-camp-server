6f08a34e5a4faa7815f5cdeb89964f16c5d7395f
Your Name <Rimon0000>
2023-06-12 13:29:59 +0600

    cart get done

--- index.js
+++ index.js
@@ -38,7 +38,17 @@ async function run() {
     })
 
 
-    //cart collection
+    //cart collection api
+    app.get("/carts", async(req, res) =>{
+      const email = req.query.email 
+      if(!email){
+        res.send([])
+      }
+      const query = { email: email };
+      const result = await cartCollection.find(query).toArray()
+      res.send(result)
+    })
+
     app.post('/carts', async(req, res) =>{
       const item = req.body
       console.log(item)
